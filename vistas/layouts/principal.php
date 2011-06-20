b04369ea8e3a6d7b5a3552ac9c0e47e859c474a3
Xionjames <xionjames@users.noreply.github.com>
2011-06-20 01:55:12 +0000

    Primeros estilos por default

--- vistas/layouts/principal.php
+++ vistas/layouts/principal.php
@@ -4,6 +4,7 @@
 		<title><?php echo APP_TITLE; ?> - <?php echo $this->pageTitle; ?></title>
         <!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" charset="utf-8"/>-->
         <link rel="stylesheet" type='text/css' href="recursos/css/plantilla.css"/>
+        <link rel="stylesheet" type='text/css' href="recursos/css/j3-styles.css"/>
 		<!--[if lt IE 9]>
 			<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js">IE7_PNG_SUFFIX=".png";</script>
 		<![endif]-->
