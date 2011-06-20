755ef38bbef4202176b9b0ad796680f0d05e5f31
Jhon.Freitez@gmail.com <Jhon.Freitez@gmail.com@aae1bf64-d5c4-40a1-ce39-4bec312d9ab8>
2011-06-20 01:55:12 +0000

    Primeros estilos por default

--- trunk/vistas/probando/index.php
+++ trunk/vistas/probando/index.php
@@ -3,3 +3,24 @@
 	<?php echo $this->variable; ?>
 </h2>
 <a href="probando/perro">click aqui</a>
+<input type="button" value="click me" class="j3-button j3-button-white" />
+<input type="button" value="click me" class="j3-button j3-button-black" />
+<input type="button" value="click me" class="j3-button j3-button-gray" />
+<input type="button" value="click me" class="j3-button j3-button-red" />
+<input type="button" value="click me" class="j3-button j3-button-pink" />
+<input type="button" value="click me" class="j3-button j3-button-orange" />
+<input type="button" value="click me" class="j3-button j3-button-blue" />
+<input type="button" value="click me" class="j3-button j3-button-green" />
+<input type="button" value="click me" class="j3-button j3-button-rosy" />
+<br /><br />
+<input type="text" value="Holaaaa" class="j3-inputText" />
+<br /><br />
+<div class="j3-select">
+	<select>
+		<option value="a">aaaaaaaaaa</option>
+		<option value="a">aaaaaaaaaa</option>
+		<option value="a">aaaaaaaaaa</option>
+		<option value="a">aaaaaaaaaa</option>
+		<option value="a">aaaaaaaaaa</option>
+	</select>
+</div>
