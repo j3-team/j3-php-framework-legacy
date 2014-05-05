6158cb03ac985195dc3d399834a4200233468e9e
j21carlos@gmail.com <jeannizama@users.noreply.github.com>
2014-05-05 16:50:06 +0000

    centrado del contenedor con css

--- vistas/layouts/principal.php
+++ vistas/layouts/principal.php
@@ -4,19 +4,10 @@
 		<title><?php echo APP_TITLE; ?> - <?php echo $this->pageTitle; ?></title>
         <!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" charset="utf-8"/>-->
         <link rel="stylesheet" type='text/css' href="recursos/css/plantilla.css"/>
-        <link rel="stylesheet" type='text/css' href="recursos/css/j3-styles.css"/>
+        <link rel="stylesheet" type='text/css' href="recursos/css/j3-styles.css"/>		
 		<!--[if lt IE 9]>
 			<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js">IE7_PNG_SUFFIX=".png";</script>
 		<![endif]-->
-        <script type="text/javascript">
-            window.onload = function(){
-                var margen = 200;
-                var widthAplicacion = 1000;
-                var widthPantalla = document.body.clientWidth - margen - widthAplicacion;
-                document.getElementById('divContenedor').style.marginLeft = 100 + (widthPantalla/2);
-                document.getElementById('divBanner').style.marginLeft = 100 + (widthPantalla/2);
-            }
-        </script>
     </head>
     <body>
         <div id="divCabecera">
